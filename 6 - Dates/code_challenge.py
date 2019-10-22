28fffa9c2cc4b5b04480e6b9e7d33b331bdcb724
Michiel <43138342+michield-immalle@users.noreply.github.com>
2019-10-22 11:19:39 +0200

    6

--- 6 - Dates/code_challenge.py
+++ 6 - Dates/code_challenge.py
@@ -2,3 +2,14 @@
 # print yesterday's date
 # ask a user to enter a date
 # print the date one week from the date entered
+from datetime import datetime, timedelta
+today = datetime.now()
+print(today)
+one_day= timedelta(days=1)
+yesterday= today - one_day
+print(yesterday)
+raindomdate = input("can u give a date(dd/mm/yyyy)")
+date = datetime.strptime(raindomdate, '%d/%m/%Y')
+week = timedelta(weeks=1)
+week_later = date + week
+print('One week later it will be: ' + str(week_later))
