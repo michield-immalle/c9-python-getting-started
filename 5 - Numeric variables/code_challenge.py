683d01b9559684629559976bb4a17ef3c3ebb413
Michiel <43138342+michield-immalle@users.noreply.github.com>
2019-10-22 11:18:41 +0200

    5

--- 5 - Numeric variables/code_challenge.py
+++ 5 - Numeric variables/code_challenge.py
@@ -4,3 +4,7 @@
 # Print 'first number + second number = answer' 
 # For example if someone enters 4 and 6 the output should read
 # 4 + 6 = 10
+First_num = input("Give me a number: ")
+Second_num = input("Give me a second number: ")
+answer = float(First_num) + float(Second_num)
+print(First_num + '+' + Second_num + '=' + str(round(answer)))
