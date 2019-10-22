b2ab86ad3ce9ecad88a36d3958347ac9a3de8e97
Michiel <43138342+michield-immalle@users.noreply.github.com>
2019-10-22 11:16:10 +0200

    4

--- 4 - String variables/code_challenge.py
+++ 4 - String variables/code_challenge.py
@@ -4,3 +4,6 @@
 # Make sure you have a space between first and last name
 # Make sure the first letter of first name and last name is uppercase
 # Make sure the rest of the name is lowercase
+First_name = input("What is ur first name: ")
+Last_name = input("What is ur last name: ")
+print(First_name.capitalize() + ' ' + Last_name.capitalize())
